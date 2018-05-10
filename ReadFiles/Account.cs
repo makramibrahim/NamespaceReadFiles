a85524d1227961dec22a48b6e249dd0218bbe5f0
makramibrahim <makram3ibrahim@gmail.com>
2018-05-10 08:58:42 -0600

    namespace readfiles

---
+++ ReadFiles/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadFiles
+{
+    class Account
+    {
+        public string Email        { get; set; }
+        public bool Active         { get; set; }
+        public DateTime CreateDate { get; set; }
+        public List<string> Roles  { get; set; }
+
+    }
+}
--- ReadFiles/Program.cs
+++ ReadFiles/Program.cs
@@ -1,12 +1,71 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
 
 namespace ReadFiles
 {
     class Program
     {
+        public static object JsonObject { get; private set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            WriteFile();
+            ReadFile();
+            Console.WriteLine("Write to the file");
+        }
+
+
+        public static void ReadFile()
+        {
+            Account account = new Account();
+            string readFile = @"C:\\Users\Mak\Desktop\wTest.txt";
+
+            StreamReader sr = new StreamReader(readFile);
+            string line = sr.ReadToEnd();
+
+                Console.WriteLine(line);
+
+        }
+
+        public static void WriteFile()
+        {
+            Account account = new Account
+            {
+                Email = "mak.ibrahim@gmail.com",
+                Active = true,
+                CreateDate = new DateTime(2018, 1, 9),
+                Roles = new List<string>
+                {
+                    "User: ilogin",
+                    "Admin: true",
+                    "Guest: false",
+                    "Share: no"
+                }
+
+            };
+
+            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+
+            string fw = @"C:\\Users\Mak\Desktop\wTest.txt";
+
+            StreamWriter writer = new StreamWriter(fw);
+            try
+            {
+                writer.WriteLine(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, "Erro Writing to the file");
+            }
+
+            writer.Close();
         }
     }
 }
